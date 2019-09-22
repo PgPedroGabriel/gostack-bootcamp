6a7a41e8780667889cd00245a98c233fba7e2e03
pg <devpedrogabriel@gmail.com>
2019-09-22 11:23:12 -0300

    Configurando nodemailer

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: 'smtp.mailtrap.com',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '',
+    pass: '',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/*
+
+Amazon SES
+Mailgun
+Sparkpost
+Mandril
+Mailtrap (dev)
+
+*/

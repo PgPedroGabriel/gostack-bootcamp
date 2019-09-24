a98e318228c706ee0f9f455104d800842890a798
pg <devpedrogabriel@gmail.com>
2019-09-24 13:47:12 -0300

    Estado e imutabilidade

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 class TechList extends Component {
 
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -9,16 +10,36 @@ class TechList extends Component {
     ]
   };
 
+  // funções para serem usadas nas tags jsx precisam ser arrow functions
+  // Só essas funções tem acesso ao THIS
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+    // o render executa novamente com as alerações de estado
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [... this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  }
+
   render() {
-
-    console.log(this.state);
-
+    //Tag fragment
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        </ul>
+      </form>
     );
   }
 }

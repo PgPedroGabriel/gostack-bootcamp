0f22f07ea51cfb871ffa895e6177a4c7e4018306
Pedro Gabriel LEVE <devpedrogabriel@gmail.com>
2019-10-23 22:45:00 -0300

    useState

--- modulo08/src/Main/index.js
+++ modulo08/src/Main/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Text } from './styles';
+import React, { useState, useEffect } from 'react';
 
 export default function Main() {
+  // ON CONST
+  // const lista = ['Nodejs', 'ReactJs', 'React Native'];
+
+  // ON USESTATE
+  const [lista, setLista] = useState([]);
+
+  // COMPONENT DID MOUNT
+  useEffect(() => {
+    setLista(['Nodejs', 'ReactJs', 'React Native']);
+  }, []);
+
   return (
     <>
-      <Text>Hello World</Text>
+      <ul>
+        {lista.map(l => {
+          return <li key={l}>{l}</li>;
+        })}
+      </ul>
     </>
   );
 }
